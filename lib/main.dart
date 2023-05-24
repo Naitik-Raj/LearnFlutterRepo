bde5997e5df35d68a0710eea79d35acf8403efa2
Naitik Raj <user@gmail.com>
2023-05-24 16:49:20 +0530

    added day2 in github

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
-import 'home.dart';
+
+import 'screens/home.dart';
 
 void main() {
   // ignore: prefer_const_constructors
   runApp(MyApp());
-}
+
 
 class MyApp extends StatelessWidget {
 
@@ -13,7 +14,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      home: Material(
+        child: Center(
+         // ignore: avoid_unnecessary_containers
+         child: Container(
+            child: const Text("Welcome to Flutter app"),
+          ),
+         ),
+      ),
+
     );
   }
 }
